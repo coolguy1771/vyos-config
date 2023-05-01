dd5a968f9f1c44a59355e8f7adc5b4dfbd1db280
Tyler Witlin <twitlin@witl.xyz>
2023-05-01 18:10:02 -0400

    fix: redo ntp config
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- config-parts/service.sh
+++ config-parts/service.sh
@@ -107,14 +107,14 @@ set service mdns repeater interface 'eth1.30'
 
 set service monitoring
 
-# NTP
-
-set service ntp allow-client address '0.0.0.0/0'
-set service ntp allow-client address '::/0'
-set service ntp server 0.us.pool.ntp.org
-set service ntp server 1.us.pool.ntp.org
-set service ntp server 2.us.pool.ntp.org
-set service ntp server 3.us.pool.ntp.org
+# NTP server
+delete service ntp allow-client
+set service ntp allow-client address '127.0.0.0/8'
+set service ntp allow-client address '10.0.0.0/8'
+set service ntp allow-client address '172.16.0.0/12'
+set service ntp allow-client address '192.168.0.0/16'
+delete service ntp server
+set service ntp server time.nist.gov
 
 # SSH
 set service ssh disable-password-authentication
