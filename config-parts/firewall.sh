7a3ac2dcdf4677ebbe225e62ae49027a77e6d1b2
Tyler Witlin <twitlin@witl.xyz>
2023-05-01 18:10:59 -0400

    feat: add cf and wg port groups
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -7,3 +7,25 @@ set firewall all-ping 'enable'
 set firewall state-policy established action 'accept'
 set firewall state-policy invalid action 'drop'
 set firewall state-policy related action 'accept'
+
+set firewall group address-group k8s_api address '10.10.254.3'
+
+# Network groups
+set firewall group network-group cloudflare-ipv4 network '173.245.48.0/20'
+set firewall group network-group cloudflare-ipv4 network '103.21.244.0/22'
+set firewall group network-group cloudflare-ipv4 network '103.22.200.0/22'
+set firewall group network-group cloudflare-ipv4 network '103.31.4.0/22'
+set firewall group network-group cloudflare-ipv4 network '141.101.64.0/18'
+set firewall group network-group cloudflare-ipv4 network '108.162.192.0/18'
+set firewall group network-group cloudflare-ipv4 network '190.93.240.0/20'
+set firewall group network-group cloudflare-ipv4 network '188.114.96.0/20'
+set firewall group network-group cloudflare-ipv4 network '197.234.240.0/22'
+set firewall group network-group cloudflare-ipv4 network '198.41.128.0/17'
+set firewall group network-group cloudflare-ipv4 network '162.158.0.0/15'
+set firewall group network-group cloudflare-ipv4 network '104.16.0.0/13'
+set firewall group network-group cloudflare-ipv4 network '104.24.0.0/14'
+set firewall group network-group cloudflare-ipv4 network '172.64.0.0/13'
+set firewall group network-group cloudflare-ipv4 network '131.0.72.0/22'
+
+# Port groups
+set firewall group port-group wireguard port '51820'
