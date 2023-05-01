36278521f2b8ac9897a256548c422b2ec9871949
Tyler Witlin <twitlin@witl.xyz>
2023-05-01 18:11:12 -0400

    fix: force NTP to router
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -1,21 +1,61 @@
 #!/bin/vbash
 
-# NAT
+
+# Forward HTTP(S) to ingress
 set nat destination rule 100 description 'HTTPS'
 set nat destination rule 100 destination port '443'
 set nat destination rule 100 inbound-interface 'eth0'
 set nat destination rule 100 protocol 'tcp'
 set nat destination rule 100 translation address '10.0.42.2'
 set nat destination rule 100 translation port '443'
+
 set nat destination rule 101 description 'HTTP'
 set nat destination rule 101 destination port '80'
 set nat destination rule 101 inbound-interface 'eth0'
 set nat destination rule 101 protocol 'tcp'
 set nat destination rule 101 translation address '10.0.42.2'
+
+# Forward Valheim Ports
 set nat destination rule 102 destination port '2456-2457,27015,27031-27036'
 set nat destination rule 102 inbound-interface 'eth0'
 set nat destination rule 102 protocol 'udp'
 set nat destination rule 102 translation address '10.10.10.52'
+
+# Force NTP
+set nat destination rule 104 description 'Force NTP for LAN'
+set nat destination rule 104 destination address '!10.10.0.1'
+set nat destination rule 104 destination port '123'
+set nat destination rule 104 inbound-interface 'eth1'
+set nat destination rule 104 protocol 'udp'
+set nat destination rule 104 translation address '10.10.0.1'
+set nat destination rule 104 translation port '123'
+
+set nat destination rule 105 description 'Force NTP for LAB'
+set nat destination rule 105 destination address '!10.10.10.1'
+set nat destination rule 105 destination port '123'
+set nat destination rule 105 inbound-interface 'eth1.10'
+set nat destination rule 105 protocol 'udp'
+set nat destination rule 105 translation address '10.10.10.1'
+set nat destination rule 105 translation port '123'
+
+set nat destination rule 106 description 'Force NTP for Wireless'
+set nat destination rule 106 destination address '!10.10.20.1'
+set nat destination rule 106 destination port '123'
+set nat destination rule 106 inbound-interface 'eth1.20'
+set nat destination rule 106 protocol 'udp'
+set nat destination rule 106 translation address '10.10.20.1'
+set nat destination rule 106 translation port '123'
+
+
+set nat destination rule 107 description 'Force NTP for Wireguard Trusted'
+set nat destination rule 107 destination address '!10.10.80.1'
+set nat destination rule 107 destination port '123'
+set nat destination rule 107 inbound-interface 'wg80'
+set nat destination rule 107 protocol 'udp'
+set nat destination rule 107 translation address '10.10.80.1'
+set nat destination rule 107 translation port '123'
+
+# LAN -> WAN masquerade
 set nat source rule 100 description 'LAN -> WAN'
 set nat source rule 100 destination address '0.0.0.0/0'
 set nat source rule 100 outbound-interface 'eth0'
